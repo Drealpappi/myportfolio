25722d1a77f296c2461e38b970e0240abe3b2cc4
FESTUS <adelekanf7@gmail.com>
2026-03-15 15:40:13 +0100

    Initial commit: Update model

--- base/models.py
+++ base/models.py
@@ -19,6 +19,5 @@ class ContactMessage(models.Model):
     subject = models.CharField(max_length= 200, null=True, blank= True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now= True)
-
     def __str__(self):
         return f"message.{self.name}-{self.email}"
